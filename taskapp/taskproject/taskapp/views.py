e826fc2f316ddcdd81305a170b363c6d41e49318
himanmenGit <study.himanmen@gmail.com>
2022-04-22 01:30:27 +0900

    basic view class, template view

--- taskapp/taskproject/taskapp/views.py
+++ taskapp/taskproject/taskapp/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Task, CheckListItem
 
 
 def index(request):
     context = {}
     return render(request, "pages/index.html", context)
+
+
+class TaskListView(TemplateView):
+    template_name = "pages/task_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        tasks = Task.objects.all()
+        context.update({
+            "tasks": tasks
+        })
+        return context
