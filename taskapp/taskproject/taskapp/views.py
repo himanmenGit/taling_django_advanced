ef4801c6d4b5f998516ca2778a38608bc1b12a23
himanmenGit <study.himanmen@gmail.com>
2022-06-08 00:10:13 +0900

    checklist create/update view

--- taskapp/taskproject/taskapp/views.py
+++ taskapp/taskproject/taskapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, ListView, DetailView
+from django.shortcuts import render, redirect, resolve_url
+from django.views import View
+from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView
 from .models import Task, CheckListItem
 from django.utils import timezone
 from django.core.paginator import Paginator
@@ -48,3 +49,54 @@ class TaskDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context["checklists"] = CheckListItem.objects.filter(task=kwargs.get("object")).all()
         return context
+
+
+class CheckListCreateView(CreateView):
+    model = CheckListItem
+    fields = ["content"]
+    template_name = "pages/checklist_create.html"
+    success_url = "/task/{0}/"
+
+    def get_success_url(self):
+        return self.success_url.format(str(self.kwargs.get("task_id")))
+
+    def form_valid(self, form):
+        data = form.save(commit=False)
+        data.task = Task.objects.get(id=self.kwargs.get("task_id"))
+        data.save()
+        return redirect(self.get_success_url())
+
+
+class CheckListUpdateView(View):
+    model = CheckListItem
+    success_url = "/task/{0}/"
+    pk_url_kwarg = "check_id"
+
+    def get_success_url(self):
+        return resolve_url("view-task", task_id=str(self.kwargs.get("task_id")))
+
+    def get_object(self):
+        return self.model.objects.get(id=self.kwargs.get(self.pk_url_kwarg))
+
+    def get(self, request, *args, **kwargs):
+        data = self.get_object()
+        data.checked = not data.checked
+        data.save()
+        return redirect(self.get_success_url())
+
+# class CheckListUpdateView(UpdateView):
+#     model = CheckListItem
+#     # fields = ["checked"]
+#     # template_name = "pages/checklist_update.html"
+#     success_url = "/task/{0}/"
+#     pk_url_kwarg = "check_id"
+#
+#     def get(self, request, *args, **kwargs):
+#         data = super().get_object()
+#         data.checked = not data.checked
+#         data.save()
+#         return redirect(self.get_success_url())
+#
+#     def get_success_url(self):
+#         return resolve_url("view-task", task_id=str(self.kwargs.get("task_id")))
+#
